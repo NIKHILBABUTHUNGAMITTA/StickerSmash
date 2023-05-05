b1c5b2db0ad1bd174dcc4474a966beb13632367c
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2023-05-05 21:46:51 +0530

    commit#6
    completed login page

--- components/Login.js
+++ components/Login.js
@@ -1,48 +1,85 @@
-import { StyleSheet, View, Image, TouchableWithoutFeedback, Text, Pressable, TextInput, Keyboard } from "react-native";
+import { StyleSheet, View, Image, TouchableWithoutFeedback, Text, Pressable, TextInput, Keyboard, TouchableOpacity } from "react-native";
 import React, { useEffect, useState } from 'react';
-import logoOne from '../assets/LogoOne.png';
+// import logoOne from '../assets/LogoOne.png';
+import { Ionicons } from '@expo/vector-icons';
+import policy from '../assets/login/policy.png';
+import microchip from '../assets/login/microchip.png';
+
+//logos
+import logo1 from '../assets/login/1.jpg';
+import logo2 from '../assets/login/2.jpg';
+import logo3 from '../assets/login/3.jpg';
+import logo4 from '../assets/login/4.jpg';
+
+const logos = [logo1,logo2,logo3,logo4];
 
 const Login = () => {
 
+  //useState
   const [toggleBt, setToggleBt] = useState(true);
+  const [username, setUsername] = useState('Sridharshree303');
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState('Asdf@123');
   const [toggleStyle, setToggleStyle] = useState({
     backgroundColor: "#EDF1F4",
     marginHorizontal: 20,
     marginTop: 10,
     marginBottom: 10,
+    height : 50,
     borderColor: 'grey',
-    borderWidth: 0,
+    alignItems : 'center',
     borderRadius: 30,
     flexDirection: toggleBt ? "row" : "row-reverse",
     flexWrap: 'wrap',
-  })
-  //password visibility
-  const [hidePwd, setHidePwd] = useState(true);
+    paddingTop : 2,
+  });
 
-  //Toggle password visibility
-  const managePasswordVisibility = () => {
-    setHidePwd(!hidePwd);
-  };
+  const [logoIndex,setLogoIndex] = useState(0);
 
-  const toggleBtn = () => {
-    // toggleStyle.flexDirection = !toggleBt ? "row" : "row-reverse";
-    setToggleStyle({ ...toggleStyle, flexDirection: !toggleBt ? "row" : "row-reverse" })
-    setToggleBt(!toggleBt);
+  //logo handler
+  const imageChange = () => {
+      setLogoIndex((logoIndex+1)%4);
   }
 
-  useEffect(() => {
+  //submit login details
+  const submit = () => {
+    alert(`${username} and ${password}`);
+  }
 
-  }, [toggleBt, toggleStyle, hidePwd])
+  //password view and hide
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const renderPasswordVisibilityToggle = () => {
+    const iconName = showPassword ? 'eye-outline' : 'eye-off-outline';
+
+    return (
+      <TouchableOpacity onPress={togglePasswordVisibility}>
+        <Ionicons style={styles.passwordPos} name={iconName} size={25} />
+      </TouchableOpacity>
+    );
+  };
+
+  //toggle button
+  const toggleBtn = () => {
+    setToggleStyle({ ...toggleStyle, flexDirection: !toggleBt ? "row" : "row-reverse" })
+    setToggleBt(!toggleBt);
+    imageChange();
+  }
+
+  //useefffect
+  useEffect(() => {
+    
+  }, [toggleBt, toggleStyle, showPassword, password,logoIndex])
 
   return (
     <>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={[styles.containerLogin, { flexDirection: "column" }]}>
-          <View style={{ flex: 0.5, backgroundColor: "black" }} >
-
-          </View>
+          <View style={{ flex: 0.4, backgroundColor: "white" }} ></View>
           <View style={{ flex: 4.5, backgroundColor: "white" }} >
-            <Image style={styles.image} source={logoOne} />
+            <Image style={styles.image} source={logos[logoIndex]} />
           </View>
 
           <View style={{ flex: 5, backgroundColor: "white" }} >
@@ -53,7 +90,6 @@ const Login = () => {
                   <>
                     <Pressable onPress={toggleBtn} disabled={!toggleBt} style={styles.button} title="Policy Number" ><Text>Policy Number</Text></Pressable>
                     <Pressable onPress={toggleBtn} disabled={toggleBt} style={StyleSheet.flatten([styles.button, styles.noBorder])} title="Pet Microchip" ><Text>Pet Microchip</Text></Pressable>
-
                   </>
                   :
                   <>
@@ -67,27 +103,43 @@ const Login = () => {
               {
                 toggleBt ?
                   <>
+                    {/* -------- username --------- */}
                     <Text style={{ fontSize: 15, paddingBottom: 5, paddingLeft: 10 }}>Policy Number</Text>
-                    <TextInput style={styles.input} onChangeText={""} value={""}></TextInput>
+                    <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                      <Image style={styles.tinyLogo} source={policy} />
+                      <TextInput style={StyleSheet.flatten([styles.input, { marginLeft: -50, paddingLeft: 60 }])} onChangeText={(uName) => setUsername(uName)} value={username}></TextInput>
+                    </View>
 
+                    {/* ----- password -------- */}
                     <Text style={{ fontSize: 15, paddingBottom: 5, paddingLeft: 10 }}>Password</Text>
-                    <TextInput style={styles.input} onChangeText={""} value={""}></TextInput>
-                    <Text style={{ textAlign: 'right', fontWeight: 500, paddingRight: 10, }} onPress={""} >Forgot Password ?</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <TextInput style={styles.input} onChangeText={pass => setPassword(pass)} secureTextEntry={!showPassword} value={password} ></TextInput>
+                      {renderPasswordVisibilityToggle()}
+                    </View>
+                    <Text style={{ textAlign: 'right', fontWeight: 500, paddingRight: 10, }} onPress={()=>{}} >Forgot Password ?</Text>
                   </>
                   :
                   <>
+                    {/* -------- username --------- */}
                     <Text style={{ fontSize: 15, paddingBottom: 5, paddingLeft: 10 }}>Pet Microchip</Text>
-                    <TextInput style={styles.input} onChangeText={""} value={""}></TextInput>
+                    <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                      <Image style={styles.tinyLogo} source={microchip} />
+                      <TextInput style={StyleSheet.flatten([styles.input, { marginLeft: -50, paddingLeft: 60 }])} onChangeText={(uName) => setUsername(uName)} value={username}></TextInput>
+                    </View>
 
+                    {/* ----- password -------- */}
                     <Text style={{ fontSize: 15, paddingBottom: 5, paddingLeft: 10 }}>Password</Text>
-                    <TextInput style={styles.input} onChangeText={``} value={''}></TextInput>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <TextInput style={styles.input} onChangeText={pass => setPassword(pass)} secureTextEntry={!showPassword} value={password} ></TextInput>
+                      {renderPasswordVisibilityToggle()}
+                    </View>
                     <Text style={{ textAlign: 'right', fontWeight: 500, paddingRight: 10, }} onPress={``} >Forgot Password ?</Text>
                   </>
               }
             </View>
 
             <View style={{ alignItems: 'center', borderWidth: 0, paddingTop: 20 }}>
-              <Pressable style={styles.loginBtn} onPress={"#"}>
+              <Pressable style={styles.loginBtn} onPress={submit}>
                 <Text style={styles.loginText}>Login</Text>
               </Pressable>
             </View>
@@ -108,14 +160,28 @@ const styles = StyleSheet.create({
     width: 393,
   },
 
+  passwordPos: {
+    marginLeft: -50,
+    overflow: "hidden",
+    position: "relative",
+  },
+
+  tinyLogo: {
+    width: 30,
+    height: 30,
+    marginLeft: 20,
+  },
+
   input: {
     height: 50,
-    paddingLeft: 20,
+    paddingLeft: 30,
     justifyContent: "center",
     borderRadius: 25,
     borderWidth: 1,
     borderColor: "grey",
     marginBottom: 5,
+    width: '100%',
+    fontSize: 17,
   },
 
   form: {
@@ -136,8 +202,8 @@ const styles = StyleSheet.create({
 
   textStyle: {
     textAlign: "center",
-    fontFamily: "",
-    fontSize: 35,
+    fontFamily: "sans-serif",
+    fontSize: 32,
     fontWeight: 600,
     margin: 10,
   },
@@ -158,7 +224,7 @@ const styles = StyleSheet.create({
     borderColor: "grey",
     justifyContent: "center",
     alignItems: 'center',
-
+    
   },
 
   noBorder: {
